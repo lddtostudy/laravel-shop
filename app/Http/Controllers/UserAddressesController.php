e9ac831fca2046b2e7360ab8f3e3a9dfbcdc2ef6
lddtostudy <11159538496@qq.com>
2020-02-23 08:19:55 +0000

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\UserAddress;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserAddressRequest;
 
 class UserAddressesController extends Controller
 {
     public function index(Request $request)
     {
-        $a = $request->user()->addresses;
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses,
         ]);
@@ -17,6 +17,21 @@ class UserAddressesController extends Controller
 
     public function create()
     {
-        return view('user_address.create_and_edit',['address'=>new UserAddress()]);
+        return view('user_addresses.create_and_edit',['address'=>new UserAddress()]);
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
     }
 }
